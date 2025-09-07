7fbbd3619e3b4076299f025838b2d06509553459
Rohit Sinha <rohit@hashgraph.com>
2025-09-07 13:07:30 -0400

    feat: typescript bindings for verification methods
    Signed-off-by: Rohit Sinha <rohit@hashgraph.com>

--- bindings/node/index.js
+++ bindings/node/index.js
@@ -1,8 +1,19 @@
-import { ts_protect_secret } from "derec-library";
+import { ts_protect_secret, ts_generate_verification_request, ts_generate_verification_response, ts_verify_share_response } from "derec-library";
 const secret_id = new Uint8Array([1, 2, 3, 4, 255]);
 const secret_data = new Uint8Array([5, 6, 7, 8, 255]);
 const channels = new BigUint64Array([1n, 2n, 3n]);
 const threshold = 2;
 const version = 1;
-console.log("ts_protect_secret: ", ts_protect_secret(secret_id, secret_data, channels, threshold, version));
+let shares = ts_protect_secret(secret_id, secret_data, channels, threshold, version);
+let some_share = shares.value.get(1);
+let some_channel = 1n;
+console.log("ts_protect_secret: ", shares.value);
+let request = ts_generate_verification_request(secret_id, version);
+console.log("ts_generate_verification_request: ", request);
+let response = ts_generate_verification_response(secret_id, some_channel, some_share, request);
+console.log("ts_generate_verification_response: ", response);
+let verification_expected_true = ts_verify_share_response(secret_id, some_channel, some_share, response);
+console.log("ts_verify_share_response (expected true): ", verification_expected_true);
+let verification = ts_verify_share_response(secret_id, 1n, shares.value.get(2), response);
+console.log("ts_verify_share_response (expected false): ", verification);
 //# sourceMappingURL=index.js.map
--- library/src/verification/mod.rs
+++ library/src/verification/mod.rs
@@ -1,7 +1,42 @@
 pub mod verification;
+use prost::Message;
 pub use verification::generate_verification_request;
 pub use verification::generate_verification_response;
 pub use verification::verify_share_response;
+
+use crate::protos::derec_proto::{VerifyShareRequestMessage, VerifyShareResponseMessage};
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn ts_generate_verification_request(
+    secret_id: &[u8],
+    version: u32,
+) -> Vec<u8> {
+    verification::generate_verification_request(secret_id, version as i32).encode_to_vec()
+}
+
+#[wasm_bindgen]
+pub fn ts_generate_verification_response(
+    secret_id: &[u8],
+    channel_id: u64,
+    share_content: &[u8],
+    request: &[u8],
+) -> Vec<u8> {
+    let request = VerifyShareRequestMessage::decode(request).unwrap();
+    verification::generate_verification_response(secret_id, &channel_id, share_content, &request).encode_to_vec()
+}
+
+#[wasm_bindgen]
+pub fn ts_verify_share_response(
+    secret_id: &[u8],
+    channel_id: u64,
+    share_content: &[u8],
+    response: &[u8],
+) -> bool {
+    let response = VerifyShareResponseMessage::decode(response).unwrap();
+    verification::verify_share_response(secret_id, &channel_id, share_content, &response)
+}
 
 #[cfg(test)]
 mod test;
