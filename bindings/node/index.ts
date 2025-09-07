fd59cd0b2bc564b062362e32ff0b42f546274894
Rohit Sinha <rohit@hashgraph.com>
2025-09-06 23:42:44 -0400

    feat: typescript test
    Signed-off-by: Rohit Sinha <rohit@hashgraph.com>

---
+++ bindings/node/index.ts
@@ -0,0 +1,9 @@
+import { ts_protect_secret } from "derec-library";
+
+const secret_id = new Uint8Array([1, 2, 3, 4, 255]);
+const secret_data = new Uint8Array([5, 6, 7, 8, 255]);
+const channels = new BigUint64Array([1n, 2n, 3n]);
+const threshold = 2;
+const version = 1;
+
+console.log("ts_protect_secret: ", ts_protect_secret(secret_id, secret_data, channels, threshold, version));
